98de8d899ba04acb8547eb1d78b634597f27e3ea
Rohaan Aslam <rohaan2.aslam@live.uwe.ac.uk>
2022-10-20 15:49:34 +0100

    Added database password

--- GUI/main2.py
+++ GUI/main2.py
@@ -11,7 +11,7 @@ def get_connection():
     conn = mysql.connector.connect(host='localhost',
                                    port='3306',
                                    user='root',
-                                   password='',
+                                   password='asdfasf',
                                    database='tkinter')
     return conn
 
