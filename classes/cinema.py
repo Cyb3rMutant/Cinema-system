263858e7ed20c475735cf77ad7d9e2be4d3c9eac
yazeed <yazeed2.abu-hummos@live.uwe.ac.uk>
2022-11-17 16:57:06 +0000

    cinema and listing

--- classes/cinema.py
+++ classes/cinema.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # ˅
+import listing
 import screen
 import listing
 from film_container import Film_container
@@ -10,27 +11,25 @@ import film
 
 
 class Cinema(object):
-    # ˅
-
-    # ˄
-
     def __init__(self, cinema_id, address):
 
         self.__cinema_id = cinema_id
 
         self.__address = address
         self.__screens = list()
+        self.__listings = list()
 
         screens = conn.select(
             "SELECT * FROM SCREEENS WHERE CINEMA_ID=%d", (self.__cinema_id,))
-
         for screen in screens:
-
             self.__screens.append(
                 screen.Screen(screen[0], screen[1], screen[2], screen[3]))
 
-        self.__listings = list()
-        ...
+        listings = conn.select(
+            "SELECT * FROM SCREENS WHERE CINEMA_ID=%d", (self.__cinema_id,))
+        for listing in listings:
+            self.__listings.append(listing.Listing(
+                listing[0], listing[1], listing[2], listing[3]))
 
     def get_cinema_id(self):
         return self.__cinema_id
--- classes/listing.py
+++ classes/listing.py
@@ -3,81 +3,53 @@
 # ˅
 import film
 import show
-
+from dbfunc import conn
 
 # ˄
 
 
 class Listing(object):
-    # ˅
-
-    # ˄
-
     def __init__(self, listing_id, date, film, cinema):
 
-        self.__listing_id = None
+        self.__listing_id = listing_id
 
-        self.__date = None
+        self.__date = date
 
-        self.__film = None
+        self.__film = film
 
-        self.__cinema = None
+        self.__cinema = cinema
 
-        self.__shows = None
+        self.__shows = list()
 
-        self.__show = []
-
-        self.__film = None
-
-        # ˅
-        pass
-        # ˄
+        shows = conn.select("SELECT * FROM SHOWS WHERE LISTING_ID=%d",
+                            self.__listing_id)
+        for show in shows:
+            self.__shows.append(
+                show.Show(show[0], show[1], show[2], show[3], show[4], show[5], show[6]))
 
     def get_listing_id(self):
-        # ˅
-        pass
-        # ˄
+        return self.__listing_id
 
     def get_date(self):
-        # ˅
-        pass
-        # ˄
+        return self.__date
 
     def get_film(self):
-        # ˅
-        pass
-        # ˄
+        return self.__film
 
     def get_cinema(self):
-        # ˅
-        pass
-        # ˄
+        return self.__cinema
 
     def get_shows(self):
-        # ˅
-        pass
-        # ˄
+        return self.__shows
 
     def set_date(self, date):
-        # ˅
-        pass
-        # ˄
+        conn.update("UPDATE LISTINGS SET LISTING_DATE=%s WHERE LISTING_ID=%d",
+                    (date, self.__listing_id,))
 
     def set_film(self, film):
-        # ˅
-        pass
-        # ˄
+        conn.update("UPDATE LISTINGS SET FILM_TITLE=%s WHERE LISTING_ID=%d",
+                    (film.get_title(), self.__listing_id,))
 
     def update_show_time(self, show_id, time):
-        # ˅
-        pass
-        # ˄
-
-    # ˅
-
-    # ˄
-
-
-# ˅
-
-# ˄
+        conn.update("UPDATE SHOWS SET SHOW_TIME=%s WHERE SHOW_ID=%d",
+                    (time, show_id,))
